085c4987f1ff51c6e7d7d227180a877277a6752a
uiste <798407452@qq.com>
2016-11-30 18:42:49 +0800

    no message

---
+++ Application/Admin/Controller/IndexController.class.php
@@ -0,0 +1,12 @@
+<?php
+namespace Admin\Controller;
+use Think\Controller;
+class IndexController extends Controller {
+    public function index(){
+    	$this->display();
+    }
+    public function haha(){
+    	
+        $this->display();
+    }
+}
